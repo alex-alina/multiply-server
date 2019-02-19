3ee78f4d3b27622d8fe2615cd8dd2b60eeea37a3
Alina <alex.alina.rusu@gmail.com>
2019-02-19 14:11:22 +0100

    completed project setup with additional dependencies and config files

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
-const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
-//don't use sync in production
-//add if clause to not sync if production
-sequelize.sync()
-  .then(() => {
-    console.log('Sequelize updated database schema')
-  })
-  .catch(console.error)
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres';
+const sequelize = new Sequelize(connectionString, {
+  define: {
+    timestamps: false,
+  }
+});
 
-module.exports = sequelize
+if (process.env.NODE_ENV != 'production') {
+  sequelize.sync()
+    .then(() => {
+      console.warn('Sequelize updated database schema');
+    })
+    .catch(console.error);
+}
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const contactsRouter = require('./contacts/routes')
+const cors = require('cors');
+// const contactsRouter = require('./contacts/routes')
+// require('./db'); => why?
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app
-// .use(cors())
-.use(bodyParser.json())
-.use(contactsRouter)
-.listen(port, () => console.log(`Listening on port ${port}`))
+  .use(cors())
+  .use(bodyParser.json())
+  // .use(contactsRouter)
+  .listen(port);
